873d67ac61d2d3272101781c036a50a228c57671
Jelle Jan Bleeker <jelle@cin-ergy.com>
2026-06-01 11:47:06 +0200

    fix: Fixed issue where signals were not forwarded to caddy

--- startup.sh
+++ startup.sh
@@ -10,5 +10,5 @@ xcaddy build $CADDY_VERSION $CADDY_MODULES
 caddy version
 caddy list-modules --packages --versions -s
 
-caddy run --config "/etc/caddy/Caddyfile" --adapter caddyfile
+exec caddy run --config "/etc/caddy/Caddyfile" --adapter caddyfile
 
